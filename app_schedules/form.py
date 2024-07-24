1c4f91119fd57d21136006e61061179b24d14192
reinhart <reinhartsoplantila@gmail.com>
2024-07-24 20:56:51 +0800

    schedule index

---
+++ app_schedules/form.py
@@ -0,0 +1,9 @@
+from django import forms
+from app_outlet.models import OutletModel
+
+class ScheduleForm(forms.Form):
+    datas=forms.ModelMultipleChoiceField(
+        queryset=OutletModel.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=True
+    )
--- app_schedules/views.py
+++ app_schedules/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from app_outlet.models import OutletModel
+from .form import ScheduleForm
 
 # Create your views here.
 def index(request):
-    OutletObject = OutletModel.objects.all()
+    # OutletObject = OutletModel.objects.all()
+    form = ScheduleForm(request.POST or None)
+    error=None
+    if request.method=='POST':
+        if form.is_valid():
+            selectedData = form.cleadned_data['data']
+            print(selectedData)
+        else:
+            error = form.errors
     context={
-        'objectdatas' : OutletObject,
+        # 'objectdatas' : OutletObject,
+        'form' : form
     }
     return render(request, 'schedule/index.html', context)
 
