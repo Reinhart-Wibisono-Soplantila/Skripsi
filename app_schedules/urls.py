7a70fc431e6c71a7eadffb854ece38662e104de8
reinhart <reinhartsoplantila@gmail.com>
2024-08-05 19:22:01 +0800

    Add Driver and Vehicle for schedule page

--- app_schedules/urls.py
+++ app_schedules/urls.py
@@ -3,10 +3,11 @@ from . import views
 
 app_name='app_schedules'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('selected-outlets/', views.viewoutlets, name='viewoutlets'),
+    path('select-outlets', views.index, name='index'),
+    path('confirm-outlets/', views.viewoutlets, name='viewoutlets'),
     path('selected-outlets/process-outlets/', views.processoutlets, name='processoutlets'),
-    path('overview/', views.overview),
-    path('vehicle/', views.vehicle),
-    path('result/', views.result),
+    path('select-vehicles/', views.vehicles, name='vehicles'),
+    path('select-drivers/', views.drivers, name='drivers'),
+    # path('result/', views.result, name='results'),
+    # path('overview/', views.overview),
 ]
--- app_schedules/views.py
+++ app_schedules/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from app_outlet.models import OutletModel
+from app_vehicle.models import VehicleModel, DriverModel
 from .algorithm.GA import GeneticAlgorithm 
 from .algorithm.SMO import SpiderMonkeyAlgorithm
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -9,13 +11,15 @@ def index(request):
     OutletObject = OutletModel.objects.all()
     error={}
     if request.method == 'POST':
-        CheckedList = request.POST.get('datas', '')
+        CheckedList = request.POST.get('selected_outlets', '')
+        print(CheckedList)
         if not CheckedList:
-            error['datas'] = "You must select at least one option."
+            error['selected_outlets'] = "You must select at least one option."
             print('error')
         if not error:
             cities = [str(x) for x in CheckedList.split(',')]
             request.session['cities'] = cities
+            messages.success(request, error['selected_outlets'])
             return redirect('app_schedules:viewoutlets')
     context={
         'error' : error,
@@ -24,43 +28,78 @@ def index(request):
     return render(request, 'schedule/index.html', context)
 
 def viewoutlets(request):
-    error={}
     # Ambil ID dari sesi
     citiesId = request.session.get('cities', [])
     OutletObject = OutletModel.objects.filter(OutletCode__in=citiesId)
     context={
-        'error' : error,
         'OutletObject' : OutletObject
     }
-    return render(request, 'schedule/selectedOutlets.html', context)
+    return render(request, 'schedule/confirm.html', context)
 
 def processoutlets(request):
     cities = request.session.get('cities', [])
-    print('cities', cities)
     # GA
-    # GA = GeneticAlgorithm()
-    # answer, genNumber  = GA.main(cities)
+    GA = GeneticAlgorithm()
+    answer, genNumber  = GA.main(cities)
     # print('answer=', answer[0])
     # print('location=', answer[1])
     # print(genNumber)
+    request.session['locations'] = answer[1]
+    request.session['distance'] = answer[0]
     
     # SMO
-    SMO = SpiderMonkeyAlgorithm()
-    location, fitness = SMO.main(cities)
-    request.session['locations'] = location
-    request.session['distance'] = fitness
+    # SMO = SpiderMonkeyAlgorithm()
+    # location, fitness = SMO.main(cities)
+    # request.session['locations'] = location
+    # request.session['distance'] = fitness
             
     # Hapus data dari sesi jika tidak diperlukan lagi
     # request.session.pop('cities_ids', None)
-    # request.session['locations'] = answer[1]
-    # request.session['distance'] = answer[0]
-    return redirect('app_schedules:viewoutlets')
+    return redirect('app_schedules:vehicles')
     # return render( request, 'schedule/selectedOutlets.html')
+    
+def vehicles(request):
+    VehicleObject = VehicleModel.objects.all()
+    error = {}
+    if request.method == 'POST':
+        
+        CheckedList = request.POST.get('selected_vehicles', '')
+        # CheckedList = request.POST.get('datas', '')
+        if not CheckedList:
+            error['selected_vehicles'] = "You must select at least one option."
+            print('error')
+        if not error:
+            Vehicles = [str(x) for x in CheckedList.split(',')]
+            request.session['vehicles'] = Vehicles
+            return redirect('app_schedules:drivers')
+    context={
+        'VehicleObject' : VehicleObject,
+    }
+    return render(request, 'schedule/vehicle.html', context)
+
+def drivers(request):
+    DriverObject = DriverModel.objects.all()
+    error = {}
+    if request.method == 'POST':
+        
+        CheckedList = request.POST.get('selected_drivers', '')
+        # CheckedList = request.POST.get('datas', '')
+        if not CheckedList:
+            error['selected_drivers'] = "You must select at least one option."
+            print('error')
+        if not error:
+            print('check :',CheckedList)
+            # Vehicles = [str(x) for x in CheckedList.split(',')]
+            # request.session['selected_drivers'] = Vehicles
+            return redirect('app_schedules:drivers')
+    context={
+        'DriverObject' : DriverObject,
+    }
+    return render(request, 'schedule/driver.html', context)
+
 def overview(request):
     return render(request, 'schedule/overview.html')
 
-def vehicle(request):
-    return render(request, 'schedule/vehicle.html')
 
 def result(request):
     return render(request, 'schedule/result.html')
