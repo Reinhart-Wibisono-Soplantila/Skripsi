89ffd1f9a4b41cbfdad2982357f24df955327f9a
reinhart <reinhartsoplantila@gmail.com>
2024-08-26 12:29:55 +0800

    dashboard

--- app_dashboard/urls.py
+++ app_dashboard/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from .views import Dashboard
 from . import views
 
 app_name="app_dashboard"
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
 ]
--- app_dashboard/views.py
+++ app_dashboard/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import View
+from app_outlet.models import OutletModel
+from app_vehicle.models import VehicleModel, DriverModel
 
-# Create your views here.
 def index(request):
-    return render(request, 'dashboard/index.html')
+    OutletObject = OutletModel.objects.exclude(OutletType='Source')
+    VehicleObject = VehicleModel.objects.all()
+    DriverObject = DriverModel.objects.all()
+    context = {
+        'OutletObject' : OutletObject,
+        'VehicleObject' : VehicleObject,
+        'DriverObject' : DriverObject,
+    }
+    return render(request, 'dashboard/index.html', context)
+# Create your views here.
+class Dashboard(View):
+    template_name = 'dashboard/index.html'
+    
+    def get(self, request):
+        OutletObject = OutletModel.objects.exclude(OutletType='Source')
+        VehicleObject = VehicleModel.objects.all()
+        DriverObject = DriverModel.objects.all()
+        context = {
+            'OutletObject' : OutletObject,
+            'VehicleObject' : VehicleObject,
+            'DriverObject' : DriverObject,
+        }
+        return render(request, self.template_name, context)
+
